651d5babb450bb16822a9d6c5e08aa369f80a280
rfmajor <rfmajor99@gmail.com>
2023-07-17 12:20:01 +0200

    Initialize the project

---
+++ src/main/java/com/rfmajor/scrabblesolver/ScrabbleSolverApplication.java
@@ -0,0 +1,13 @@
+package com.rfmajor.scrabblesolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrabbleSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrabbleSolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rfmajor/scrabblesolver/ScrabbleSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfmajor.scrabblesolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrabbleSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
