a62eeb09e2dbfe61c3bd4f1a66a613d736b5fcc8
rfmajor <rfmajor99@gmail.com>
2023-07-27 14:24:09 +0200

    Modify alphabet to be a local variable

--- src/main/java/com/rfmajor/scrabblesolver/common/LetterSetMapper.java
+++ src/main/java/com/rfmajor/scrabblesolver/common/LetterSetMapper.java
@@ -1,6 +1,5 @@
 package com.rfmajor.scrabblesolver.common;
 
-import com.rfmajor.scrabblesolver.common.exceptions.AlphabetTooLargeException;
 import org.springframework.stereotype.Component;
 
 import java.math.BigInteger;
@@ -9,20 +8,11 @@ import java.util.Set;
 
 @Component
 public class LetterSetMapper {
-    private final Alphabet alphabet;
-
-    public LetterSetMapper(Alphabet alphabet) {
-        if (alphabet.size() >= Long.SIZE) {
-            throw new AlphabetTooLargeException("The alphabet is too large: " + alphabet.size());
-        }
-        this.alphabet = alphabet;
+    public String mapLetterSetToHexVector(Set<Character> letters, Alphabet alphabet) {
+        return Long.toHexString(mapLetterSetToLong(letters, alphabet)).toUpperCase();
     }
 
-    public String mapLetterSetToHexVector(Set<Character> letters) {
-        return Long.toHexString(mapLetterSetToLong(letters)).toUpperCase();
-    }
-
-    public long mapLetterSetToLong(Set<Character> letters) {
+    public long mapLetterSetToLong(Set<Character> letters, Alphabet alphabet) {
         long sum = 0;
         for (char letter : letters) {
             sum += Math.pow(2, alphabet.getIndex(letter));
@@ -30,12 +20,12 @@ public class LetterSetMapper {
         return sum;
     }
 
-    public Set<Character> mapToSet(String hexVector) {
+    public Set<Character> mapToSet(String hexVector, Alphabet alphabet) {
         long decoded = new BigInteger(hexVector, 16).longValue();
-        return mapToSet(decoded);
+        return mapToSet(decoded, alphabet);
     }
 
-    public Set<Character> mapToSet(long hexVector) {
+    public Set<Character> mapToSet(long hexVector, Alphabet alphabet) {
         Set<Character> letterSet = new HashSet<>();
         for (int i = 0; i < alphabet.size(); i++) {
             if (((hexVector >> i) & 1) == 1L) {
@@ -45,17 +35,17 @@ public class LetterSetMapper {
         return letterSet;
     }
 
-    public int addLetterToVector(int vector, char letter) {
+    public int addLetterToVector(int vector, char letter, Alphabet alphabet) {
         int index = alphabet.getIndex(letter);
         return vector | (1 << index);
     }
 
-    public int removeLetterFromVector(int vector, char letter) {
+    public int removeLetterFromVector(int vector, char letter, Alphabet alphabet) {
         int index = alphabet.getIndex(letter);
         return vector & ~(1 << index);
     }
 
-    public boolean containsLetter(int vector, char letter) {
+    public boolean containsLetter(int vector, char letter, Alphabet alphabet) {
         int index = alphabet.getIndex(letter);
         return ((vector >> index) & 1) == 1;
     }
--- src/test/java/com/rfmajor/scrabblesolver/common/LetterSetMapperTest.java
+++ src/test/java/com/rfmajor/scrabblesolver/common/LetterSetMapperTest.java
@@ -13,96 +13,105 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LetterSetMapperTest {
     private LetterSetMapper letterSetMapper;
+    private Alphabet alphabet;
 
     @BeforeEach
     void setUp() {
-        letterSetMapper = new LetterSetMapper(
-                new Alphabet(
-                        mapStringToLettersList("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"),
-                        Collections.emptyList(),
-                        Collections.emptyList()
-                ));
+        letterSetMapper = new LetterSetMapper();
+        alphabet = new Alphabet(
+                mapStringToLettersList("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"),
+                Collections.emptyList(),
+                Collections.emptyList()
+        );
     }
 
     @Test
     void givenAllLettersWith32LettersInAlphabet_whenMapLetterSetToHexVector_thenReturn8Fs() {
-        String mapped = letterSetMapper.mapLetterSetToHexVector(mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"));
+        String mapped = letterSetMapper.mapLetterSetToHexVector(
+                mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"), alphabet);
         assertEquals("FFFFFFFF", mapped);
     }
 
     @Test
     void givenAllLettersWith32LettersInAlphabet_whenMapLetterSetToLong_thenReturn8Fs() {
-        long mapped = letterSetMapper.mapLetterSetToLong(mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"));
+        long mapped = letterSetMapper.mapLetterSetToLong(
+                mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż"), alphabet);
         assertEquals((long) Math.pow(2, 32) - 1, mapped);
     }
 
     @Test
     void givenNoLettersWith32LettersInAlphabet_whenMapLetterSetToHexVector_thenReturn0() {
-        String mapped = letterSetMapper.mapLetterSetToHexVector(mapStringToLettersSet(""));
+        String mapped = letterSetMapper.mapLetterSetToHexVector(
+                mapStringToLettersSet(""), alphabet);
         assertEquals("0", mapped);
     }
 
     @Test
     void givenNoLettersWith32LettersInAlphabet_whenMapLetterSetToLong_thenReturn0() {
-        long mapped = letterSetMapper.mapLetterSetToLong(mapStringToLettersSet(""));
+        long mapped = letterSetMapper.mapLetterSetToLong(
+                mapStringToLettersSet(""), alphabet);
         assertEquals(0L, mapped);
     }
 
     @Test
     void givenABCDEFLettersWith32LettersInAlphabet_whenMapLetterSetToHexVector_thenReturn16D() {
-        String mapped = letterSetMapper.mapLetterSetToHexVector(mapStringToLettersSet("abcdef"));
+        String mapped = letterSetMapper.mapLetterSetToHexVector(
+                mapStringToLettersSet("abcdef"), alphabet);
         assertEquals("16D", mapped);
     }
 
     @Test
     void givenABCDEFLettersWith32LettersInAlphabet_whenMapLetterSetToLong_thenReturn365() {
-        long mapped = letterSetMapper.mapLetterSetToLong(mapStringToLettersSet("abcdef"));
+        long mapped = letterSetMapper.mapLetterSetToLong(
+                mapStringToLettersSet("abcdef"), alphabet);
         assertEquals(365L, mapped);
     }
 
     @Test
     void givenInvalidLetterWith32LettersInAlphabet_whenMapLetterSetToHexVector_thenThrowException() {
-        assertThrows(AlphabetLetterNotPresentException.class, () -> letterSetMapper.mapLetterSetToHexVector(Set.of('v')));
+        assertThrows(AlphabetLetterNotPresentException.class,
+                () -> letterSetMapper.mapLetterSetToHexVector(Set.of('v'), alphabet));
     }
 
     @Test
     void givenInvalidLetterWith32LettersInAlphabet_whenMapLetterSetToLong_thenThrowException() {
-        assertThrows(AlphabetLetterNotPresentException.class, () -> letterSetMapper.mapLetterSetToLong(Set.of('v')));
+        assertThrows(AlphabetLetterNotPresentException.class,
+                () -> letterSetMapper.mapLetterSetToLong(Set.of('v'), alphabet));
     }
 
     @Test
     void given8FsHexVectorWith32LettersInAlphabet_whenMapToSet_thenReturnAllLetters() {
-        Set<Character> mapped = letterSetMapper.mapToSet("FFFFFFFF");
+        Set<Character> mapped = letterSetMapper.mapToSet("FFFFFFFF", alphabet);
         assertTrue(mapped.containsAll(mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż")));
     }
 
     @Test
     void given8FsLongWith32LettersInAlphabet_whenMapToSet_thenReturnAllLetters() {
-        Set<Character> mapped = letterSetMapper.mapToSet((long) Math.pow(2, 32) - 1);
+        Set<Character> mapped = letterSetMapper.mapToSet((long) Math.pow(2, 32) - 1, alphabet);
         assertTrue(mapped.containsAll(mapStringToLettersSet("aąbcćdeęfghijklłmnńoóprsśtuwyzźż")));
     }
 
     @Test
     void given0HexVectorWith32LettersInAlphabet_whenMapToSet_thenReturnEmptySet() {
-        Set<Character> mapped = letterSetMapper.mapToSet("0");
+        Set<Character> mapped = letterSetMapper.mapToSet("0", alphabet);
         assertTrue(mapped.isEmpty());
     }
 
     @Test
     void given0LongWith32LettersInAlphabet_whenMapToSet_thenReturnEmptySet() {
-        Set<Character> mapped = letterSetMapper.mapToSet(0L);
+        Set<Character> mapped = letterSetMapper.mapToSet(0L, alphabet);
         assertTrue(mapped.isEmpty());
     }
 
     @Test
     void given16DHexVectorWith32LettersInAlphabet_whenMapToSet_thenReturnABCDEF() {
-        Set<Character> mapped = letterSetMapper.mapToSet("16D");
+        Set<Character> mapped = letterSetMapper.mapToSet("16D", alphabet);
         assertTrue(mapped.containsAll(mapStringToLettersSet("abcdef")));
     }
 
     @Test
     void given16DLongWith32LettersInAlphabet_whenMapToSet_thenReturnABCDEF() {
-        Set<Character> mapped = letterSetMapper.mapToSet(365L);
+        Set<Character> mapped = letterSetMapper.mapToSet(365L, alphabet);
         assertTrue(mapped.containsAll(mapStringToLettersSet("abcdef")));
     }
 
