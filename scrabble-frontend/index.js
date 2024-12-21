b75cd92b6985aee55aaaaccd79384920cc59f349
rfmajor <rfmajor99@gmail.com>
2024-12-21 14:45:31 +0100

    test

--- scrabble-frontend/index.js
+++ scrabble-frontend/index.js
@@ -4,13 +4,14 @@ const COLORS = {
     "doubleWord": "#e6c3a8",
     "tripleWord": "#e80d0d",
     "borderColor": "#000000",
-    "letterBackground": "#ea924a",
-    "default": "#095e12"
+    "empty": "#095e12",
+    "board": "#043608"
 }
 const BOARD_LENGTH = 15
-const CELL_SIZE = 40
-const CELL_BORDER_SIZE = 1
-const BOARD_SIZE_PX = 16 * CELL_BORDER_SIZE + 15 * CELL_SIZE
+const CELL_SIZE_PX = 40
+const BORDER_SIZE_PX = 1
+const BOARD_PADDING_PX = 40
+const BOARD_SIZE_PX = 16 * BORDER_SIZE_PX + 15 * CELL_SIZE_PX
 const CELLS = new Set()
 
 function getCellCoordsAndWidth(x, y) {
@@ -18,10 +19,10 @@ function getCellCoordsAndWidth(x, y) {
     // eg. x = 0 -> border + 0 * cell_size
     // x = 1 -> 2 * border + 1 * cell_size
     // x = 2 -> 3 * border + 2 * cell_size
-    let cell_x = (x + 1) * CELL_BORDER_SIZE + x * CELL_SIZE
-    let cell_y = (y + 1) * CELL_BORDER_SIZE + y * CELL_SIZE
-    let cell_w = CELL_SIZE
-    let cell_h = CELL_SIZE
+    let cell_x = BOARD_PADDING_PX + (x + 1) * BORDER_SIZE_PX + x * CELL_SIZE_PX
+    let cell_y = BOARD_PADDING_PX + (y + 1) * BORDER_SIZE_PX + y * CELL_SIZE_PX
+    let cell_w = CELL_SIZE_PX
+    let cell_h = CELL_SIZE_PX
     return {x: cell_x, y: cell_y, w: cell_w, h: cell_h}
 }
 
@@ -31,10 +32,10 @@ async function fillCell(x, y, color, canvas) {
     let cell = getCellCoordsAndWidth(x, y)
 
     // border_x = cell_x - border
-    let border_x = cell.x - CELL_BORDER_SIZE
-    let border_y = cell.y - CELL_BORDER_SIZE
-    let border_w = cell.w + 2 * CELL_BORDER_SIZE
-    let border_h = cell.h + 2 * CELL_BORDER_SIZE
+    let border_x = cell.x - BORDER_SIZE_PX
+    let border_y = cell.y - BORDER_SIZE_PX
+    let border_w = cell.w + 2 * BORDER_SIZE_PX
+    let border_h = cell.h + 2 * BORDER_SIZE_PX
 
     // border
     ctx.fillStyle = COLORS["borderColor"]
@@ -63,6 +64,11 @@ window.onload = async function() {
     let specialFields = await fetch('./assets/specialFields.json')
         .then(response => response.json())
 
+    // board padding
+    let ctx = CANVAS.getContext("2d")
+    ctx.fillStyle = COLORS["board"]
+    ctx.fillRect(0, 0, BOARD_SIZE_PX + 2 * BOARD_PADDING_PX, BOARD_SIZE_PX + 2 * BOARD_PADDING_PX)
+
     let specialFieldsPopulated = new Set()
 
     for (const key of Object.keys(specialFields)) {
@@ -75,13 +81,22 @@ window.onload = async function() {
     for (let i = 0; i < BOARD_LENGTH; i++) {
         for (let j = 0; j < BOARD_LENGTH; j++) {
             if (!(specialFieldsPopulated.has(`${i}, ${j}`))) {
-                await fillCell(i, j, COLORS["default"], CANVAS)
+                await fillCell(i, j, COLORS["empty"], CANVAS)
             }
         }
     }
 
-    await putLetter(7, 7, 'w', CANVAS)
+    ctx.font = "20px arial"
+    ctx.fillStyle = "white"
+    ctx.textAlign = "left"
+    ctx.textBaseline = "top"
+    let cell = getCellCoordsAndWidth(2, 0)
+    let letter = "C"
+    let text = ctx.measureText(letter);
+    ctx.fillText(letter, cell.x + ((cell.w / 2) - (text.width / 2)), cell.y - CELL_SIZE_PX)
+
     await putLetter(8, 7, 'e', CANVAS)
+    await putLetter(7, 7, 'w', CANVAS)
     await putLetter(9, 7, 'ź', CANVAS)
     await putLetter(10, 7, 'blank', CANVAS)
     await putLetter(11, 7, 'e', CANVAS)
