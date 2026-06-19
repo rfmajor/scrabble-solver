5a780d5f8376647a4988bea3cf0778a99a84139d
Filip Major <filip.major@tesco.com>
2026-06-19 17:57:54 +0200

    Rename type p. 2

--- dictionary-converter-c/src/gaddag.h
+++ dictionary-converter-c/src/gaddag.h
@@ -2,7 +2,7 @@
 #ifndef GADDAG
 #define GADDAG
 
-void gaddag_convert(const char *dictionary_f, const char *output_f, hashmap_t *mapped_alphabet, const int max_word,
+void gaddag_convert(const char *dictionary_f, const char *output_f, hashmap *mapped_alphabet, const int max_word,
                     const int gzip);
 
 #endif // !GADDAG
